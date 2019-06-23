1be14646ab408b30935c81ce1ebd4b7dfbd49023
Joseph Young <jsyoung0891@gmail.com>
2019-06-23 18:32:39 +0100

    added game file with ideas for mechanics

---
+++ src/main/java/Game.java
@@ -0,0 +1,38 @@
+import enemies.Boss;
+import enemies.NormalEnemy;
+import player.Barbarian;
+import player.Player;
+import rooms.Room;
+
+public class Game {
+
+
+
+    public Game(){
+
+        gameStart();
+    }
+
+    public void gameStart(){
+        System.out.println("Game starting");
+        // player selects a character/weapon somehow. text input with big if statement for multiple choices?
+        // multiple players added to party array?
+        // player selects a direction (this wont matter)
+        // generate room
+        // logic to determine which type of room. increased chance for normalenemy etc
+        // add this to room
+        // player chooses to attack or run perhaps?
+
+        // for each enemy in room, its attacks a player. more players? random attack
+        // keep attacking until someone dies
+        // reward?
+        // select direction and start again
+
+
+        // game ends if player(s) die or..
+        // array of bosses? when defeated, delete from array. empty array = game win
+    }
+
+
+
+}
