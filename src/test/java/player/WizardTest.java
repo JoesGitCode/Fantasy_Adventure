b284752193286013dd4b3c13fd80a45a93358a87
Joseph Young <jsyoung0891@gmail.com>
2019-06-23 16:57:43 +0100

    fixed failing tests

--- src/test/java/player/WizardTest.java
+++ src/test/java/player/WizardTest.java
@@ -16,7 +16,7 @@ public class WizardTest {
     public void before(){
         currentSpell = new Spell("Spark", 4, 10, SpellTarget.SINGLE);
 
-        wizard = new Wizard("Glindorf", 100, currentSpell);
+        wizard = new Wizard("Glindorf", 20, 100, currentSpell);
     }
 
     @Test
